8776bfb3e2fa555edfc006906d3e106c8ccb8175
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-02-03 11:18:35 -0300

    Mudança de metódo map para loop for

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ function adicionarAmigo() {
         alert('Por favor, insira um nome válido.');
     } else {
         amigos.push(nomeAmigo);
-        listaAmigos.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
-        document.getElementById('amigo').value = ''
+
+        listaAmigos.innerHTML = ''
+        for (x = 0; x < amigos.length; x++) {
+            listaAmigos.innerHTML += `<li>${amigos[x]}</li>` ;
+        }
+
     }
-    
+
 
 };
 
-function sortearAmigo(){
+function sortearAmigo() {
     let amigoSorteado = getElementById('resultado');
 
-
-    
+    amigoSorteado.innerHTML = ``
 }
