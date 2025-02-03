5e005124169423363302cb4ef270c034172d5150
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-02-03 10:21:01 -0300

    Função adicionar amigos

---
+++ app.js
@@ -0,0 +1,11 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+    let listaAmigos = document.getElementById('listaAmigos');
+
+    if (nomeAmigo == '') {
+        alert('Por favor, insira um nome válido.');
+    } 
+
+}
