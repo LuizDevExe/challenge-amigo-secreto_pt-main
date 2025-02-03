30c6330a1c75245d7c2e44b01d85235f97deb13f
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2025-02-03 12:19:46 -0300

    Adições de regras de sorteio e disabled após sorteio

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let amigos = [];
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById('amigo').value;
     let listaAmigos = document.getElementById('listaAmigos');
-
     if (nomeAmigo == '') {
         alert('Por favor, insira um nome válido.');
     } else {
@@ -11,16 +10,22 @@ function adicionarAmigo() {
 
         listaAmigos.innerHTML = ''
         for (x = 0; x < amigos.length; x++) {
-            listaAmigos.innerHTML += `<li>${amigos[x]}</li>` ;
+            listaAmigos.innerHTML += `<li>${amigos[x]}</li>`;
         }
 
+        document.getElementById('amigo').value = '';
     }
 
-
-};
+}
 
 function sortearAmigo() {
-    let amigoSorteado = getElementById('resultado');
+    let amigoSorteado = document.getElementById('resultado');
+    if (amigos.length == 0) {
+        alert('Por favor, para sortear um amigo primeiramente digite algum nome');
+    } else {
+        amigoSorteado.innerHTML = `<li>O amigo secreto sorteado é:${amigos[Math.floor(Math.random() * amigos.length)]}</li>`;
+        listaAmigos.innerHTML = '';
+        document.querySelector('.button-draw').setAttribute('disabled', true);
+    }
 
-    amigoSorteado.innerHTML = ``
 }
